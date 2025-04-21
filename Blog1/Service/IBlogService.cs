3f10ba7a178abfc4a5c4581709495a09b7d8d2d5
gabriel2443 <gabriel.iorga200@gmail.com>
2025-04-21 20:29:50 +0200

    boilerplate code and some logic

---
+++ Blog1/Service/IBlogService.cs
@@ -0,0 +1,10 @@
+﻿namespace Blog1.Models.Service;
+
+public interface IBlogService
+{
+    public  Task<List<Blog>> GetAllBlogs();
+    public Task<Blog?> GetBlogById(int id);
+
+    public Task CreateBlog(Blog blog);
+
+}
